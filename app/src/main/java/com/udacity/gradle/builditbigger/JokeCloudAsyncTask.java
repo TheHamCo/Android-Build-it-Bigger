5a2b77387b549ed1eecaa54276e6338408d0af7f
Michael Dijamco <mdd233@cornell.edu>
2016-08-08 14:05:49 -0400

    feat: Decouple intent logic between MainActivity and JokeCloudAsyncTask

--- app/src/main/java/com/udacity/gradle/builditbigger/JokeCloudAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/JokeCloudAsyncTask.java
@@ -1,7 +1,5 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
 
 import com.example.mdd23.myapplication.joketellercloud.myApi.MyApi;
@@ -12,17 +10,14 @@ import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
 import java.io.IOException;
 
-import co.dijam.michael.joketellerandroid.JokeActivity;
-
 /**
  * Created by mdd23 on 8/8/2016.
  */
-public class JokeCloudAsyncTask extends AsyncTask<Context, Void, String> {
+public class JokeCloudAsyncTask extends AsyncTask<Void, Void, String> {
     private static MyApi myApiService = null;
-    private Context context;
 
     @Override
-    protected String doInBackground(Context... params) {
+    protected String doInBackground(Void... params) {
         if (myApiService==null){ //Singleton - do this only once
             MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
                     .setRootUrl("http://192.168.1.14:8080/_ah/api")
@@ -38,19 +33,10 @@ public class JokeCloudAsyncTask extends AsyncTask<Context, Void, String> {
             myApiService = builder.build();
         }
 
-        context = params[0];
-
         try{
             return myApiService.getJoke().execute().getJoke();
         } catch (IOException e ){
             return e.getMessage();
         }
-    }
-
-    @Override
-    protected void onPostExecute(String jokeResult) {
-        Intent jokeIntent = new Intent(context, JokeActivity.class);
-        jokeIntent.putExtra(JokeActivity.JOKE_EXTRA, jokeResult);
-        context.startActivity(jokeIntent);
     }
 }
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,11 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import co.dijam.michael.joketellerandroid.JokeActivity;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -19,7 +22,14 @@ public class MainActivity extends ActionBarActivity {
         tellJokeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new JokeCloudAsyncTask().execute(MainActivity.this);
+                (new JokeCloudAsyncTask(){
+                    @Override
+                    protected void onPostExecute(String jokeResult) {
+                        Intent jokeIntent = new Intent(MainActivity.this, JokeActivity.class);
+                        jokeIntent.putExtra(JokeActivity.JOKE_EXTRA, jokeResult);
+                        startActivity(jokeIntent);
+                    }
+                }).execute();
             }
         });
     }
