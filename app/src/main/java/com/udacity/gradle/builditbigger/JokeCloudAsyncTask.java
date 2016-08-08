bcfd44423b48910be514b74a930f618a2ead9136
Michael Dijamco <mdd233@cornell.edu>
2016-08-08 13:40:04 -0400

    feat: Add and live test GCE

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/JokeCloudAsyncTask.java
@@ -0,0 +1,54 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v4.util.Pair;
+import android.widget.Toast;
+
+import com.example.mdd23.myapplication.joketellercloud.myApi.MyApi;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+import java.io.IOException;
+
+/**
+ * Created by mdd23 on 8/8/2016.
+ */
+public class JokeCloudAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+    private static MyApi myApiService = null;
+    private Context context;
+
+    @Override
+    protected String doInBackground(Pair<Context, String>... params) {
+        if (myApiService==null){ //Singleton - do this only once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                    .setRootUrl("http://192.168.1.14:8080/_ah/api")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> request) throws IOException {
+                            // Turn off compression when running against local devappserver
+                            request.setDisableGZipContent(true);
+                        }
+                    });
+            //END options for devapp server
+
+            myApiService = builder.build();
+        }
+
+        context = params[0].first;
+        String name = params[0].second;
+
+        try{
+            return myApiService.sayHi(name).execute().getData();
+        } catch (IOException e ){
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Toast.makeText(context, result, Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,11 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.JokeTeller;
 
@@ -18,6 +21,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button testApiButton = (Button) findViewById(R.id.test_api_button);
+        testApiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new JokeCloudAsyncTask().execute(new Pair<Context, String>(MainActivity.this, "Michael"));
+            }
+        });
     }
 
 
